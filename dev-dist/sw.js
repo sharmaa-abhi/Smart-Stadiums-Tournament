396a24cbe1142c03c67b743b99a8450286e2fc54
sharmaa-abhi <sharmaa24434@gmail.com>
2026-07-10 09:30:12 +0530

    add service

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-caf3a6a6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p0g95tpq4qs"
+    "revision": "0.l4kp2pghgms"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
